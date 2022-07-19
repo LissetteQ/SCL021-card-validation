e72a2d4ecf213831f3331717eaeca46cea0ce2a3
LissetteQ <lissetteq123@gmail.com>
2022-07-18 23:23:06 -0400

    validacion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
-import validator from "./validator.js";
-function mostrarAlerta() {
-  var numeroTarjeta = document.getElementById("ncard");
-  alert("Tarjeta bloqueada " + numeroTarjeta.value);
-  validator.isValid(numeroTarjeta.value);
+/*importamos nuestra función validator y maskify de un módulo exclusivo para estas funciones*/
+import validator from './validator.js';
+console.log(validator);
+// Input número de tarjeta
+document.getElementById("nCard").addEventListener('keyup', (e)  => {
+  let valorInput = e.target.value
+  document.getElementById("nCard").value = valorInput
+  // Se dejan de permitir espacios en blancos
+  .replace(/\s/g, '')
+  // Permitimos que se escriban sólo carácteres numéricos
+  .replace(/\D/g, '')
+})
+
+/*función password para ocultar el Clave*/
+let y = document.getElementById("pass");
+y.addEventListener("click", btnPassword);
+
+function btnPassword() {
+  let p = document.getElementById("myInput");
+  if (p.type === "password") {
+    p.type = "text";
+  } else {
+    p.type = "password";
+  }
 }
 
-{
-  var button = document.querySelector(".button");
-  button.addEventListener("click", mostrarAlerta);
+/* declaramos variable para el botón Validar */
+let z = document.getElementById("valid");
+z.addEventListener("click", btnValid);
 
-  console.log(el.value);
-}
+/*creamos función para traer string del número de tarjeta y transformarlo en array con un .split y simultáneamente 
+convertirlo en valor númerico con un .map */
+function btnValid() {
+    let ccNumber = document.getElementById("nCard").value;
+    console.log(typeof(ccNumber))
+    let creditCardNumber = ccNumber.split("").map(Number);
+    console.log(creditCardNumber)
+
+  /* a través de una condicional aplicamos función validator a nuestra variable creditCardNumber y en simultáneo
+  llamamos a la función Maskify para retornar string enmascarado */
+
+    if (validator.isValid(creditCardNumber)==true) {
+        document.getElementById('validacion').innerHTML = 'Tu tarjeta es válida';
+        document.getElementById('nCard').value = validator.maskify(creditCardNumber);
+        alert("Tarjeta Valida  "+nCard.value);
+        } 
+        else {
+        document.getElementById('validacion').innerHTML = 'Tu tarjeta es inválida, vuelve a intentarlo';
+        document.getElementById('nCard').value = validator.maskify(creditCardNumber);
+        alert("Tarjeta Bloqueada  "+nCard.value);
+        }
+}    
