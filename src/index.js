4d98f17d77627e51b8ede73b4d99b6e491271e3c
LissetteQ <lissetteq123@gmail.com>
2022-07-21 15:00:29 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 /*importamos nuestra función validator y maskify de un módulo exclusivo para estas funciones*/
-import validator from './validator.js';
-console.log(validator);
+import validator from "./validator.js";
 // Input número de tarjeta
-document.getElementById("nCard").addEventListener('keyup', (e)  => {
-  let valorInput = e.target.value
+document.getElementById("nCard").addEventListener("keyup", (e) => {
+  let valorInput = e.target.value;
   document.getElementById("nCard").value = valorInput
-  // Se dejan de permitir espacios en blancos
-  .replace(/\s/g, '')
-  // Permitimos que se escriban sólo carácteres numéricos
-  .replace(/\D/g, '')
-})
+    // Se dejan de permitir espacios en blancos
+    .replace(/\s/g, "")
+    // Permitimos que se escriban sólo carácteres numéricos
+    .replace(/\D/g, "");
+});
 
 /*función password para ocultar el Clave*/
 let y = document.getElementById("pass");
@@ -31,22 +30,24 @@ z.addEventListener("click", btnValid);
 /*creamos función para traer string del número de tarjeta y transformarlo en array con un .split y simultáneamente 
 convertirlo en valor númerico con un .map */
 function btnValid() {
-    let ccNumber = document.getElementById("nCard").value;
-    console.log(typeof(ccNumber))
-    let creditCardNumber = ccNumber.split("").map(Number);
-    console.log(creditCardNumber)
+  let ccNumber = document.getElementById("nCard").value;
+  //console.log(typeof(ccNumber))
+  let creditCardNumber = ccNumber.split("").map(Number);
+  //console.log(creditCardNumber)
 
   /* a través de una condicional aplicamos función validator a nuestra variable creditCardNumber y en simultáneo
   llamamos a la función Maskify para retornar string enmascarado */
 
-    if (validator.isValid(creditCardNumber)==true) {
-        document.getElementById('validacion').innerHTML = 'Tu tarjeta es válida';
-        document.getElementById('nCard').value = validator.maskify(creditCardNumber);
-        alert("Tarjeta Valida  "+nCard.value);
-        } 
-        else {
-        document.getElementById('validacion').innerHTML = 'Tu tarjeta es inválida, vuelve a intentarlo';
-        document.getElementById('nCard').value = validator.maskify(creditCardNumber);
-        alert("Tarjeta Bloqueada  "+nCard.value);
-        }
-}    
+  if (validator.isValid(creditCardNumber) == true) {
+    document.getElementById("validacion").innerHTML = "Tu tarjeta es válida";
+    document.getElementById("nCard").value =
+      validator.maskify(creditCardNumber);
+    alert("Tarjeta Valida  " + document.getElementById("nCard").value);
+  } else {
+    document.getElementById("validacion").innerHTML =
+      "Tu tarjeta sigue bloqueada, vuelve a intentarlo";
+    document.getElementById("nCard").value =
+      validator.maskify(creditCardNumber);
+    alert("Tarjeta Bloqueada  " + document.getElementById("nCard").value);
+  }
+}
--- src/validator.js
+++ src/validator.js
@@ -5,47 +5,44 @@ const validator = {
     let numSum = 0;
     let value;
     /* acá se cambia el valor de los values pares y se suman los dígitos si la suma es mayor o igual a 10 */
-    for (let i = 0; i < 16; ++i) {
-          if (i % 2 == 0) {
-              value = 2*nCard[i];
-              if (value >= 10) {
-                value = (Math.floor(value / 10) + (value % 10));  
-              }
-          } 
-          else {
-              value = +nCard[i];
-          }
-          numSum += value;
+    for (let i = 0; i < nCard.length; ++i) {
+      if (i % 2 == 0) {
+        value = 2 * nCard[i];
+        if (value >= 10) {
+          value = Math.floor(value / 10) + (value % 10);
+        }
+      } else {
+        value =+ nCard[i];
+      } 
+      numSum += value;
     }
-    
-    /* si el módulo de la suma dividido en 10 es 0 entonces la tarjeta es válida */      
-    if (numSum%10==0) {
-          return true;
-        }
-        else {
-          return false;
-        }
-
+    /* si el módulo de la suma dividido en 10 es 0 entonces la tarjeta es válida */
+    if (numSum % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
   },
 
   maskify: (nCard) => {
     /* acá el array se divide en dos: primero se divide todo menos los últimos 4 números para enmascarar */
-    let maskedCc = nCard.slice(0, nCard.length-4);
+    let maskedCc = nCard.slice(0, nCard.length - 4);
     let arrayMasked = [];
-    let lastFour = nCard.slice(-4).join('');
-    
-    if (nCard.length <= 4){
+    //console.log(nCard.slice(-4));
+    let lastFour = nCard.slice(-4);
+
+    if (nCard.length <= 4) {
       return lastFour;
     }
-    /* .push agrega por cada número a enmascarar un # al array vacío arrayMasked */  
-    for (let i = 0; i < maskedCc.length; i++){
-      arrayMasked.push('#');
+    /* .push agrega por cada número a enmascarar un # al array vacío arrayMasked */
+    for (let i = 0; i < maskedCc.length; i++) {
+      arrayMasked.push("#");
     }
-    
-    let joinMasked = arrayMasked.join('');
-    
+
+    let joinMasked = arrayMasked.join("");
+
     return joinMasked + lastFour;
-    }
-}
+  },
+};
 
 export default validator;
